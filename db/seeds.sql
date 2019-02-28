9d74a3b2314e5140fa33b45518b7f816c008d896
Naomi Johnson <naomikaiena@gmail.com>
2019-02-28 17:30:15 -0500

    updates

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burger (burger_name)VALUES('Classic');
-INSERT INTO burger (burger_name) VALUES ('The BTL');
-INSERT INTO burger (burger_name, devoured) VALUES ('Pizza Burger', true);
-INSERT INTO burger (burger_name, devoured) VALUES ('Veggie Burger', true);
-INSERT INTO burger (burger_name, devoured) VALUES ('Black Bean', true);
-INSERT INTO burger (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name)VALUES('Classic');
+INSERT INTO burgers (burger_name) VALUES ('The BTL');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean', true);
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
